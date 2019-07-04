73e2099dfcb359a0d43e9b9203c4cfeb7fb8911d
Jelly999 <aalto98@outlook.com>
2019-07-04 14:27:17 +0300

    Login fragment incomplete

--- app/src/main/java/com/example/ht1/MainActivity.java
+++ app/src/main/java/com/example/ht1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ht1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Help!");
+    }
+    public void login(View V){
+        
     }
 }
