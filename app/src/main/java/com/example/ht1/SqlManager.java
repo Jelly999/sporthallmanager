1b978a3d5b30d4031d465963798c06301cb249aa
p5014 <leipuri.junnuinen@gmail.com>
2019-07-12 10:56:53 +0300

    List initializers and fetch from sql

--- app/src/main/java/com/example/ht1/SqlManager.java
+++ app/src/main/java/com/example/ht1/SqlManager.java
@@ -372,6 +372,8 @@ public class SqlManager {
         if (cursor.moveToFirst()) {
             do {
                 Reservation reservation = new Reservation();
+
+                //TODO Reservation olion setterit
             } while (cursor.moveToNext());
         }
         cursor.close();
