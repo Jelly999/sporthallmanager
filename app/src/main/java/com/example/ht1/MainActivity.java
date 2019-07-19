d59ea58f81ff8f530c09231cd9c83156de16723b
Jelly999 <aalto98@outlook.com>
2019-07-19 15:03:03 +0300

    Main activity code removed

--- app/src/main/java/com/example/ht1/MainActivity.java
+++ app/src/main/java/com/example/ht1/MainActivity.java
@@ -43,37 +43,7 @@ public class MainActivity extends AppCompatActivity {
         }
         objectInitalizationTest();
         // Testi loppu
-        {
-            ArrayList<ExampleItem> exampleList = new ArrayList<>();
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 3", "Line 4"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 5", "Line 6"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 7", "Line 8"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 9", "Line 10"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 11", "Line 12"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 13", "Line 14"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 15", "Line 16"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 17", "Line 18"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 19", "Line 20"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 21", "Line 22"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 23", "Line 24"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 25", "Line 26"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 27", "Line 28"));
-            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 29", "Line 30"));
-/*
-            mRecyclerView = findViewById(R.id.recyclerView);
-            //mRecyclerView.setHasFixedSize(true);
-            mLayoutManager = new LinearLayoutManager(this);
-            mAdapter = new ExampleAdapter(exampleList);
 
-            mRecyclerView.setLayoutManager(mLayoutManager);
-            mRecyclerView.setAdapter(mAdapter);
-*/        } //Recycle view test
-
-    //TODO launch login fragment
-
-        // TEMPORARY TEST
-        //launchMainMenu();
         launchlogin();
 
         // TODO THIS IS HOW TO CALL JSON
