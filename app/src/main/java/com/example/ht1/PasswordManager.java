88bf8d2f347dc8ecbe28b8dec7d31336c9d7ee69
Jelly999 <aalto98@outlook.com>
2019-07-19 14:03:27 +0300

    Manage users code

--- app/src/main/java/com/example/ht1/PasswordManager.java
+++ app/src/main/java/com/example/ht1/PasswordManager.java
@@ -49,7 +49,7 @@ public class PasswordManager {
         return false;
     }
 
-    private static boolean passwordIsCompliant(String str) {
+    public static boolean passwordIsCompliant(String str) {
         char ch;
         boolean capitalFlag = false;
         boolean lowerCaseFlag = false;
