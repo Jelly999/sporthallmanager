b65230470952c7146685626a9a653f4aeab4d502
Jelly999 <aalto98@outlook.com>
2019-07-23 10:39:46 +0300

    manage hall export buttons

--- app/src/main/java/com/example/ht1/MainActivity.java
+++ app/src/main/java/com/example/ht1/MainActivity.java
@@ -79,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         JSONManager jsonManager = new JSONManager(this);
         jsonManager.JSONTEST();
     }
+    public void CSVcall(View v){
+        //TODO csv
+    }
 
     public void launchMainMenu() {
         MainMenuFragment joinevent = new MainMenuFragment();
